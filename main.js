cc4c235e7edee96561b5abad446f69ffd7ffeb02
Simone Mauro <simon.mauro@virgilio.it>
2022-02-11 18:14:06 +0100

    aggiunta dinamica dei todo completata

--- main.js
+++ main.js
@@ -24,7 +24,35 @@ const app = new Vue({
             }
         ],
 
-        newToDo: ""
+        //provando a mettere l'oggetto da aggiungere separatamente, l'oggetto viene aggiunto ma il testo viene sovrascritto ogni volta
+        /* newToDo: {
+            text: "",
+            done: false
+        }, */
 
+        newTodoText: ""
+
+    },
+    methods: {
+
+        addTask(){
+
+            if(this.newTodoText == ""){
+                alert("Inserisci un valore valido");
+            }else{
+                //this.newToDo.text = this.newTodoText;
+                this.itemList.push({
+                    text: this.newTodoText,
+                    done: false
+                });
+                this.newTodoText = "";
+                
+            }
+            
+        },
+
+        taskDone(index){
+            this.itemList[index].done = !this.itemList[index].done;
+        }
     }
 });
